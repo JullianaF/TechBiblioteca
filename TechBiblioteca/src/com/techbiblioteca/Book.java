fc8b67b937581ba8b83fe3ee2b782223a1c7745e
JullianaF <125063577+JullianaF@users.noreply.github.com>
2024-09-07 16:37:02 -0300

    TechBiblioteca - 1° Fase
    TechBiblioteca é um sistema de gerenciamento de livros em Java, utilizando lista encadeada para armazenamento e algoritmos Bubble Sort e Quick Sort para ordenação. Permite adicionar, listar e ordenar livros por título ou autor, oferecendo uma interface de linha de comando intuitiva.

---
+++ TechBiblioteca/src/com/techbiblioteca/Book.java
@@ -0,0 +1,30 @@
+package com.techbiblioteca;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + title + ", Autor: " + author + ", Ano: " + year;
+    }
+}
